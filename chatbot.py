29c1a0de1585671a54063bf32d1bbce95ad026c3
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-29 17:40:23 +0000

    email change

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,8 @@
 import pandas as pd
 import numpy as np
 import pickle as pk
-from sklearn.feature
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
+import r
