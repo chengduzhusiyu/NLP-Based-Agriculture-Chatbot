0564025eb9fec40eac5f9dab3d1bf1e8083f2f2b
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-29 20:56:42 +0000

    Updated the python-language-server feedstock.

--- chatbot.py
+++ chatbot.py
@@ -5,4 +5,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import GaussianNB
-import r
+import re
+from nltk.stem.porter import PorterStemmer
+from sklearn.model_selection import train_test_split
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
